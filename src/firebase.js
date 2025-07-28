6eb4ffc6e535f3685fbd343a7e91f0685aadc948
Anush Shrestha <137639753+Anush980@users.noreply.github.com>
2025-07-28 16:16:36 +0545

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCPduUyDei2aVuJf5ekN3LaplTnJz-Apdg",
-    authDomain: "portfolio-80b40.firebaseapp.com",
-    projectId: "portfolio-80b40",
-    storageBucket: "portfolio-80b40.appspot.com",
-    messagingSenderId: "98735530914",
-    appId: "1:98735530914:web:f53291e48e7110223348fb",
-    measurementId: "G-9KCKN7LGQW"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "1",
+    measurementId: ""
 };
 
 // Initialize Firebase
